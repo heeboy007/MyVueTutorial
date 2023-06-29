84f458f96ab39e59dd6afc023d64d104b0e845d8
geezhardname <heeboy007@naver.com>
2023-06-29 12:43:30 +0000

    function : feature basic page using localStorage

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,39 @@
+<template>
+	<header>
+		<h1>TODO it!</h1>
+	</header>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+	body {
+		text-align: center;
+		background-color: #F6F6F8;
+	}
+	
+	input {
+		border-style: groove;
+		width: 200px;
+	}
+	
+	button {
+		border-style: groove;
+	}
+	
+	.shadow {
+		box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
+	}
+</style>
+
+<style scoped>
+	h1 {
+		color: #2F3B52;
+		font-weight: 900;
+		margin: 2.5rem 0 1.5rem;
+	}
+</style>
